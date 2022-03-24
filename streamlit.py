3215bc656890f49485572221e138e83b77af6e1a
basketking <nemethhaz@gmail.com>
2022-03-24 14:48:08 +0100

    Latest

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-df_finalstats = pd.read_excel('https://github.com/basketking/FITEQ_matches/blob/main/finalstats.xlsx?raw=true')
-df_teq_score = pd.read_excel('https://github.com/basketking/FITEQ_matches/blob/main/teq_score.xlsx')
+df_finalstats = pd.read_csv('https://github.com/basketking/FITEQ_matches/raw/main/finalstats.csv', error_bad_lines=False)
 
 
-st.table(df_finalstats)
+df_teq_score = pd.read_csv('https://github.com/basketking/FITEQ_matches/blob/main/teq_score.csv', error_bad_lines = False)
+
+
+st.dataframe(df_finalstats)
